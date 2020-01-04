051bbc68df2dfcbc03230561de5453f8c4a40ace
jangui <jaimitino@gmail.com>
2020-01-04 16:55:48 -0500

    all done

--- Game.py
+++ Game.py
@@ -8,38 +8,52 @@ class Snake:
     def __init__(self, x, y):
         self.x = randint(0, x-1)
         self.y = randint(0, y-1)
-        self.snake = [[self.x,self.y]]
+        self.body = [[self.x,self.y]]
+        self.tail_prev_pos = self.body[-1].copy()
 
     def move(self, x, y):
-        for i in range(len(self.snake)):
-            self.snake[i][0] += x
-            self.snake[i][1] += y
-        self.x = self.snake[0][0]
-        self.y = self.snake[0][1]
+        self.tail_prev_pos = self.body[-1].copy()
+
+        #move body
+        body = self.body.copy()
+        for i in range(1, len(self.body)):
+            body[i] = self.body[i-1].copy()
+        self.body = body
+
+        #move head
+        self.body[0][0] += x
+        self.body[0][1] += y
+
+        self.x = self.body[0][0]
+        self.y = self.body[0][1]
+
+    def grow(self):
+        self.body.append(self.tail_prev_pos)
 
 class Game:
     def __init__(self):
         self.s = Settings()
         self.snake = Snake(self.s.game_x, self.s.game_y)
         self.food = [None, None]
-        self.reward = 0
         self.place_food()
+        self.points = 0
+        self.num_actions = 4
+
 
     def place_food(self):
         food_x = randint(0, self.s.game_x-1)
         food_y = randint(0, self.s.game_y-1)
-        """
-        while [food_x, food_y] not in self.snake.snake:
+        while [food_x, food_y] in self.snake.body:
             food_x = randint(0, self.s.game_x)
             food_y = randint(0, self.s.game_y)
-        """
         self.food = [food_x, food_y]
 
     def reset(self):
         self.done = False
         self.snake = Snake(self.s.game_x, self.s.game_y)
         self.place_food()
-        return (self.snake.snake, self.food)
+        self.points = 0
+        return (self.snake.body, self.food)
 
     def step(self, action):
         if action == 0: #move right
@@ -51,6 +65,8 @@ class Game:
         elif action == 3: #move down
             self.snake.move(0,-1)
 
+        reward = 0
+
         if (self.snake.x == self.s.game_x) or (self.snake.y == self.s.game_y):
             #snake hit wall (case 1)
             self.done = True
@@ -59,34 +75,36 @@ class Game:
             self.done = True
         elif (self.snake.x == self.food[0]) and (self.snake.y == self.food[1]):
             #snake got food
-            self.reward = self.s.food_reward
+            reward = self.s.food_reward
+            self.points += reward
             self.place_food()
-        elif (self.snake.x, self.snake.y) in self.snake.snake[1:]:
+            self.snake.grow()
+        elif [self.snake.x, self.snake.y] in self.snake.body[1:]:
             #snake hit body
-            self.reward = self.s.body_reward
+            reward = self.s.body_reward
             self.done = True
 
-        new_state = (self.snake.snake, self.food)
-        return (new_state, self.reward)
+        new_state = (self.snake.body, self.food)
+        return (new_state, reward)
 
     def render(self):
-        print(self.snake.snake, self.food, self.done)
         if self.done:
             return
-            if cv2.waitKey(500) & 0xFF == ord("q"):
-                return
         else:
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 return
+
         board = np.zeros((self.s.game_x, self.s.game_y, 3), dtype=np.uint8)
         food_color = (0, 255, 0)
         snake_color = (255, 175, 0)
-        """
-        for i in range(len(self.snake.snake)):
-            board[self.snake.snake[i][0]][self.snake.snake[i][1]] = snake_color
-        """
-        board[self.snake.x][self.snake.y] = snake_color
+
+        #loop over snake rendering whole body
+        for i in range(len(self.snake.body)):
+            board[self.snake.body[i][0]][self.snake.body[i][1]] = snake_color
+
+        #board[self.snake.x][self.snake.y] = snake_color
         board[self.food[0]][self.food[1]] = food_color
+
         img = Image.fromarray(board, "RGB")
         img = img.resize((300,300))
         cv2.imshow("image", np.array(img))
--- main.py
+++ main.py
@@ -4,33 +4,26 @@ from Game import Game
 import time
 
 env = Game()
-#num_actions = env.num_actions
-num_actions = 4
-#observation_space = env.observation_space.shape
+num_actions = env.num_actions
 
-episode_rewards = [] #for collecting stats
-
-episodes = 1
+episodes = 20
 render_period = 1
 render = True
 
 for episode in range(episodes):
     state = env.reset()
-    #episode_reward = 0 #for collecting stats
 
     while not env.done:
         action = random.randint(0, num_actions)
         new_state, reward = env.step(action)
         if render and (episode % render_period == 0):
             env.render()
-            time.sleep(0.1)
+            #time.sleep(0.1) #helps view game at normal speed
 
         state = new_state
-        #episode_reward += reward
 
 """
-    ##### STATS #####
-    episode_rewards.append(episode_reward)
-
-    if episode > ro
+state is a tuple containing the coordinates
+of the snake's body and the coordinates of
+the food's location
 """
