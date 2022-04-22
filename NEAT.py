dc3ae5ee94ba9186ea857af4f91f7d665b57db59
jangui <jaimitino@gmail.com>
2022-04-21 23:58:33 -0400

    speciation and mutation implemented

--- NEAT.py
+++ NEAT.py
@@ -15,10 +15,8 @@ class Node:
         self.activated = False
         self.identification = identification
         self.node_type = node_type
-        self.in_connections = []
-        self.out_connections = []
-
-
+        self.in_connections: set[int] = {}
+        self.out_connections: set[int] = {}
 
 class Connection:
     """ class for connections between nodes """
@@ -33,8 +31,9 @@ class NEAT:
     def __init__(self, num_agents: int, num_inputs: int, num_outputs: int):
         """ init for neat algorithm """
         self.num_agents = num_agents
-        self.connections: dict[int, set[int]] = {}
+        self.connections: dict[int, dict[int, int]] = {}
         self.create_agents(num_inputs, num_outputs)
+        self.current_innovation = 0
 
         # hyperparameters
         self.node_mutation_rate = 0.01
@@ -45,15 +44,22 @@ class NEAT:
         """ take the required steps for advancing a generation """
         self.speciate_agents()
         self.select_fit_agents()
-        self.crossover()
+        #self.crossover()
         self.mutate()
 
     def create_agents(self, num_inputs: int, num_outputs: int):
         """ create original agents """
         self.agents: list[Agents] = []
+
         for i in range(self.num_agents):
             agent = Agent(num_inputs, num_outputs)
-            agent.mutate()
+
+            # give agent a random mutation
+            if random.random() > 0.5:
+                self.innovation_number = agent.add_connection(self.connections, self.innovation_number)
+            else:
+                agent.add_node(self.connections)
+
             self.agents.append()
 
     def speciate_agents(self):
@@ -64,7 +70,9 @@ class NEAT:
             new_specie = True
             # check if agent is part of an existing specie
             for specie in species:
-                if agent.is_same_species(specie[0]):
+                representative_specie = specie[0]
+                speciation_difference = agent.speciate_difference(representative_specie)
+                if speciation_difference < self.speciation_threshold:
                     specie.append(agent)
                     new_specie = False
                     break
@@ -85,13 +93,17 @@ class NEAT:
         self.species = fit_species
         self.agents = fit_agents
 
-    def mutate(self):
+    def mutate(self, current_innovation):
         """ mutate agents based on mutation rates """
         for agent in agents:
             if random.random() > self.node_mutation_rate:
-                agent.add_node(self.connections)
+                innovation_number = agent.add_node(self.connections, self.current_innovation)
             if random.random() > self.connection_mutation_rate:
-                agent.add_connection(self.connections)
+                innovation_number = agent.add_connection(self.connections, self.current_innovation)
+
+            # update innovation number
+            if innovation_number > self.current_innovation:
+                self.current_innovation = innovation_number
 
     def crossover(self):
         """ cross over agents from same species """
@@ -177,51 +189,121 @@ class Agent:
             nodes.append(Node(i+num_inputs, NodeType.INPUT))
         return nodes
 
-    def mutate(self):
-        """ mutate agent. either add node or connection """
-        if random.random() > 0.5:
-            self.add_connection()
-        else:
-            self.add_node()
 
-    def add_connection(self,connections: dict[int, set[int]]):
+    def add_connection(self, connections: dict[int, set[int]], current_innovation: int) -> int:
         """ add a connection between two existing nodes """
+        # select two nodes
+        in_node, out_node = self.get_two_random_nodes()
 
-        return True
+        # check if they already have an existing connection
+        while in_node.identifier in out_node.out_connections:
+            # reselect nodes until non connected nodes
+            in_node, out_node = self.get_two_random_nodes()
 
-    def add_node(self):
-        """ add a node between two already connected nodes """
 
-        # check for existing connection
-        existing_connection = None
-        for connection in inNode.inConnections:
-            if connection.outNode.identification == outNode.identification:
-                existing_connection = connection
+        # get innovation number
+        innovation_number = self.get_innovation_number(connections, in_node, out_node, current_innovation)
 
-        # fail to add node between nodes that aren't connected
-        if not existing_connection:
-            return False
+        # set the innovation number for this connection
+        connections[out_node.identifer][in_node.identifier] = innovation_number
 
-        # create new node
-        new_node_id = len(self.nodes)+1
-        new_node = Node(new_node_id, NodeType.HIDDEN)
-        self.nodes.append(new_node)
+        # create connection
+        connection = Connection(in_node, out_node, innovation_number)
+        self.connections.append(connection)
+        return innovation_number
 
-        # disable old connection
+    def get_innovation_number(self, connections: list[Connection], in_node: Node, out_node: Node, current_innovation: int) -> int:
+        """ get the innovation number for the connection between out node and in node """
+        out_node_connections = connections[out_node.identifier]
+
+        # check if connection exists
+        if in_node.identifier in out_node_connections.keys():
+            innovation_number = out_node_connections[in_node.identifier]
+            return innovation_number
+
+        # if connection doesnt exist, then new innovation!
+        return current_innovation + 1
+
+    def get_two_random_nodes(self) -> (Node, Node):
+        """ select two random nodes """
+        node1_index = random.randint(0, len(self.nodes))
+        node2_index = random.randin(0, len(self.nodes))
+        while node2_index == node1_index:
+            node2_index = random.randin(0, len(self.nodes))
+        node1 = self.nodes[node1_index]
+        node2 = self.nodes[node2_index]
+        return (node1, node2)
+
+    def add_node(self, connections: list[Connection], current_innovation: int):
+        """
+        add a node between two already connected nodes
+        disbale the existing connection and add two new connections to and from the new node
+        """
+        # randomly select and existing connection
+        existing_connection_index = random.randint(0, len(self.connections))
+        existing_connection = self.connections[existing_connection_index]
+
+        # disable connection
         existing_connection.enabled = False
 
-        # add in connection
-        innovation_number = len(self.connections) + 1
-        inConnection = Connection(newNode, outNode, innovation_number)
-        inConnection.weight = 1
-        new_node.inConnections.append(inConnection)
-        out_node.outConnections.append(inConnection)
+        # get nodes from existing connection
+        in_node = existing_connection.in_node
+        out_node = existing_connection.out_node
 
-        # add out connection
-        innovation_number += 1
-        outConnection = Connection(newNode, outNode, innovation_number)
-        outConnection.weight = 1
-        new_node.outConnections.append(outConnection)
-        inNode.inConnections.append(outConnection)
+        # create new node
+        node_id = len(self.nodes) + 1
+        new_node = Node(node_id, NodeType.HIDDEN)
+
+        # create new connection to new node from original out node
+        innovation_number = self.get_innovation_number(connections, new_node, out_node, current_innovation)
+        to_connection = Connection(new_node, out_node, innovation_number)
+        to_connection.weight = 1 # implementation detail from original NEAT paper
+        self.connections.append(to_connection)
+
+        # create new connection from new node into original in node
+        innovation_number = self.get_innovation_number(connections, in_node, new_node, current_innovation)
+        from_connection = Connection(new_node, out_node, innovation_number)
+        from_connection.weight = existing_connection.weight # implementation detail from original NEAT paper
+        self.connections.append(from_connection)
+
+    def speciation_difference(self, other_agent: Agent, speciation_weights: list[int]) -> bool:
+        """ get the speciation difference between the other agent and this one """
+        # get the max number of genes between the two species
+        maximum_genes = max(len(self.connections), len(other_agent.connections))
+
+        # get the total excess genes
+        last_innovation = self.connections[-1].innovation_number
+        other_agent_last_innovation = other_agent[-1].innovation_number
+        excess_genes = abs(last_innovation - other_agent_last_innovation)
+
+        # get the agent without excess genes
+        non_excess_agent = self
+        excess_agent = other_agent
+        if excess_genes == other_agent_last_innovation:
+            non_excess_agent = other_agent
+            excess_agent = self
+
+        # get the disjoin and weight difference between genes
+        disjoint_genes = 0
+        joint_genes = 0
+        joint_genes_weight_diff = 0
+        for i in range(len(non_excess_agent.connections)):
+            agent1_connection = non_excess_agent.connections[i]
+            agent2_connection = excess_agent.connections[i]
+
+            if agent1_connection.innovation_number == agent2_connection.innovaton_number:
+                difference = agent1_connection.innovation_number - agent2_connection.innovation_number
+                joint_genes_weight_diff += abs(difference)
+                joint_genes += 1
+            else:
+                disjoint_genes += 1
+
+        # calculate speciation difference
+        joint_genes_avg_weight_diff = joint_genes_weight_diff / joint_genes
+        c1, c2, c3 = speciation_weights
+        excess_genes_difference =
+        speciation_difference = (((c1*excess_genes) + (c2*disjoint_genes)) / maximum_genes ) + c3*joint_genes_avg_weight_diff
+        return speciation_difference
+
 
 
